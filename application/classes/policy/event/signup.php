e44498f7de2c7ddd81b797e1d91f8600428942b7
Matt Hollis <matthollis@gmail.com>
2012-04-02 18:41:45 -0500

    Added (empty) policy classes for events and characters

---
+++ application/classes/policy/event/signup.php
@@ -0,0 +1,1 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');class Policy_Event_Signup extends Policy {	public function execute(Model_ACL_User $user, array $extras = NULL)	{		// This is not a very good policy yet		return TRUE;	}}

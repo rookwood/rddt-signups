9e4e2eee3efdb1ba2dab240651bd03da347ed706
Matt Hollis <matthollis@gmail.com>
2012-04-02 21:21:45 -0500

    More line ending fixes... changing database module branch for bug fix

--- application/classes/policy/character/remove.php
+++ application/classes/policy/character/remove.php
@@ -1,1 +1,11 @@
-<?php defined('SYSPATH') or die('No direct access allowed.');class Policy_Character_Remove extends Policy {	public function execute(Model_ACL_User $user, array $extras = NULL)	{		// This is not a very good policy yet		return TRUE;	}}
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+class Policy_Character_Remove extends Policy {
+
+	public function execute(Model_ACL_User $user, array $extras = NULL)
+	{
+		// This is not a very good policy yet
+		return TRUE;
+	}
+
+}

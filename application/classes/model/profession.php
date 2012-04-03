9a7129b05e00d955bc3454101b1779997cae582c
Matt Hollis <somehollis@gmail.com>
2012-04-03 10:41:42 -0500

    Adding validation helper

--- application/classes/model/profession.php
+++ application/classes/model/profession.php
@@ -1,2 +1,32 @@
-<?php defined('SYSPATH') or die('No direct access allowed.');/** * Profession-related data */class Model_Profession extends ORM {	// Profession constants	const WARRIOR      = 1;	const RANGER       = 2;	const MONK         = 3;	const NECROMANCER  = 4;	const ELEMENTALIST = 5;	const MESMER       = 6;	const RITUALIST    = 7;	const ASSASSAIN    = 8;	const DERVISH      = 9;	const PARAGON      = 10;	// Relationships
-	protected $_has_many = array('characters' => array());}
+<?php defined('SYSPATH') or die('No direct access allowed.');
+/**
+ * Profession-related data
+ */
+class Model_Profession extends ORM {
+
+	// Profession constants
+	const WARRIOR      = 1;
+	const RANGER       = 2;
+	const MONK         = 3;
+	const NECROMANCER  = 4;
+	const ELEMENTALIST = 5;
+	const MESMER       = 6;
+	const RITUALIST    = 7;
+	const ASSASSAIN    = 8;
+	const DERVISH      = 9;
+	const PARAGON      = 10;
+	
+	// Relationships
+	protected $_has_many = array('characters' => array());
+	
+	/**
+	 * Returns array of all current professions
+	 *
+	 * @return  array
+	 */
+	public static function list()
+	{	
+		return array('warrior', 'ranger', 'monk', 'necromancer', 'elementalist', 'mesmer', 'ritualist', 'assassain', 'dervish', 'paragon');
+	}
+	 
+}

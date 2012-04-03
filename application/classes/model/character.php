8ec538de4739bf8b22e52f3618def4319121ef46
Matt Hollis <somehollis@gmail.com>
2012-04-03 10:49:15 -0500

    Adding validation rules for character creation

--- application/classes/model/character.php
+++ application/classes/model/character.php
@@ -7,6 +7,24 @@ class Model_Character extends ORM {
 	// Relationships
 	protected $_belongs_to = array('user' => array());
 		
+	// Validation rules
+	public function rules()
+	{
+		return array(
+			'name' => array(
+				array('not_empty'),
+				array('min_length', array(':value', 3)),
+				array('max_length', array(':value', 19)),
+				// To be valid names in game, must start with a letter, contain no numbers, and no more than one consecutive space
+				array('regex', array('^[a-zA-Z]+( [a-zA-Z]+)*$')),
+				array(array($this, 'character_name_available')),
+			),
+			'profession' => array(
+				array(array($this, 'valid_profession'))	,
+			),
+		);
+	}
+	
 	/**
 	 * Create a new character
 	 *
@@ -30,4 +48,25 @@ class Model_Character extends ORM {
 		return $this->values($values, $expected)->create();
 	}
 	
+	/**
+	 * Check if character name is already in use
+	 *
+	 * @param  string  Name of character to be checked
+	 * @return bool
+	 */
+	public function character_name_available($name)
+	{
+		return ORM::factory('character', array('name', $name))->loaded();
+	}
+	
+	/**
+	 * Check if profession provided is present in the game
+	 *
+	 * @param  string  Name of profession to be checked
+	 * @return bool
+	 */
+	public function valid_profession($profession)
+	{
+		return in_array($profession, Model_Profession::list());
+	}
 }

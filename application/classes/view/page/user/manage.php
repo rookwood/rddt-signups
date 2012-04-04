f1cb35f92ff1eaffbe88206160e4242bfec86847
Matt Hollis <somehollis@gmail.com>
2012-04-04 15:00:57 -0500

    Adding timezone support to user profile management.

--- application/classes/view/page/user/manage.php
+++ application/classes/view/page/user/manage.php
@@ -1,1 +1,35 @@
-<?php defined('SYSPATH') or die('No direct access allowed.');class View_Page_User_Manage extends Abstract_View_Page {	public $title = 'Profile Editor';	public function action()	{		return Route::url('default', array('controller' => 'user', 'action'     => 'manage'));	}}
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+class View_Page_User_Manage extends Abstract_View_Page {
+
+	public $title = 'Profile Editor';
+
+	public function action()
+	{
+		return Route::url('default', array('controller' => 'user', 'action' => 'manage'));
+	}
+
+	/**
+	 * @return  array   Formatted list of timezones for use in <select>
+	 */
+	public function timezone_list()
+	{
+		$current_timezone = $this->user->timezone;
+		
+		foreach (Date::$timezone_list as $value => $name)
+		{
+			
+			if ($key == $current_timezone)
+			{
+				$out[] = array('value' => $value, 'name' => $name, 'selected' => TRUE);
+			}
+			else
+			{
+				$out[] = array('value' => $value, 'name' => $name);
+			}
+		}
+		
+		return $out;		
+	}
+
+}

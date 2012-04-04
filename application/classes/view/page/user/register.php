c2cabbb741a6b64e1c5dde5dd38fe3effa675a05
Matt Hollis <somehollis@gmail.com>
2012-04-04 14:58:28 -0500

    Finishing registration view class timezone support.

--- application/classes/view/page/user/register.php
+++ application/classes/view/page/user/register.php
@@ -1,5 +1,47 @@
-<?php defined('SYSPATH') or die('No direct access allowed.');class View_Page_User_Register extends Abstract_View_Page {	public $title = 'Registration Form';
-	public $errors;		public $values;		public function action()
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+class View_Page_User_Register extends Abstract_View_Page {
+
+	public $title = 'Registration Form';
+
+	/**
+	 * @var   array   List of validation errors (if any)
+	 */
+	public $errors;
+	
+	/**
+	 * @var   array   List of form values to be prefilled (if any)
+	 */
+	public $values;
+	
+	/**
+	 * @return  string  Form action URL
+	 */
+	public function action()
 	{
 		return Route::url('default', array('controller' => 'user', 'action' => 'register'));
-	}}
+	}
+	
+	/**
+	 * @return  array   Formatted list of timezones for use in <select>
+	 */
+	public function timezone_list()
+	{
+		$current_timezone = $this->user->timezone;
+		
+		foreach (Date::$timezone_list as $value => $name)
+		{
+			
+			if ($key == $current_timezone)
+			{
+				$out[] = array('value' => $value, 'name' => $name, 'selected' => TRUE);
+			}
+			else
+			{
+				$out[] = array('value' => $value, 'name' => $name);
+			}
+		}
+		
+		return $out;		
+	}
+}

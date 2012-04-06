0a6683da66f12498208d02c4ebc79b8b5a7bac32
Matt Hollis <matthollis@gmail.com>
2012-04-06 11:11:25 -0500

    removing typo filename

--- application/classes/model/singup.php
+++
@@ -1,2 +0,0 @@
-<?php defined('SYSPATH') or die('No direct access allowed.');/** * Event sign-up model */class Model_Signup extends ORM {	// Relationships
-	protected $_has_many = array(		'events'     => array(),		'characters' => array(),	);}

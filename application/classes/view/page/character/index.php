3d8d5ce603363176915b6ee535311cd206046f87
Matt Hollis <somehollis@gmail.com>
2012-04-09 12:46:03 -0500

    Adding comments

--- application/classes/view/page/character/index.php
+++ application/classes/view/page/character/index.php
@@ -12,6 +12,11 @@ class View_Page_Character_Index extends Abstract_View_Page {
 	 */
 	public $count = FALSE;
 	
+	/**
+	 * Returns a list of the current user's characters
+	 *
+	 * @return  array  Characters
+	 */
 	public function character_list()
 	{
 		foreach ($this->characters as $character)
@@ -25,6 +30,11 @@ class View_Page_Character_Index extends Abstract_View_Page {
 		return $out;
 	}
 	
+	/**
+	 * URL for the form action to add a new character or FALSE if not allowed
+	 *
+	 * @return  mixed  (string) URL or (bool) FALSE
+	 */	
 	public function character_add_link()
 	{
 		if ($this->user->can('character_add'))

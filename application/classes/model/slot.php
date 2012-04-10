56332bd69ffe2a3f3b113a2949e32f4eb9405923
Matt Hollis <somehollis@gmail.com>
2012-04-10 09:48:57 -0500

    Better caching within function scope instead of class scope

--- application/classes/model/slot.php
+++ application/classes/model/slot.php
@@ -4,10 +4,6 @@
  * will belong to potentially many builds through a m:n relationship called functions.
  */
 class Model_Slot extends ORM {
-
-	// Used to cache a few statuses.  Saves us up to 14 database hits
-	public static $cancelled;
-	public static $standby;
 	
 	// Relationships
 	protected $_has_many = array(
@@ -30,20 +26,23 @@ class Model_Slot extends ORM {
 	
 	public function slots_filled(Model_Event $event)
 	{
-		// Poor man's status caching
-		if ( ! self::$cancelled)
-			self::$cancelled = ORM::factory('status', array('name' => 'cancelled'))->id;
+		// Saving status IDs in static cache reduces number of database considerably
+		static $cache = array();
 		
-		if ( ! self::$standby)
-			self::$standby = ORM::factory('status', array('name' => 'standby'))->id;
+		// Poor man's status caching
+		if ( ! isset($cache['cancelled']))
+			$cache['cancelled'] = ORM::factory('status', array('name' => 'cancelled'))->id;
+		
+		if ( ! isset($cache['standby']))
+			$cache['standby'] = ORM::factory('status', array('name' => 'standby'))->id;
 		
 		// Count how many slots are taken up by sign-ups
 		$slots_filled = DB::select(array('COUNT("id")', 'count'))
 			->from('signups')
 			->where('event_id',       '=', $event->id)
 			->and_where('slot_id',    '=', $this->id)
-			->and_where('status_id', '!=', self::$cancelled)
-			->and_where('status_id', '!=', self::$standby)
+			->and_where('status_id', '!=', $cache['cancelled'])
+			->and_where('status_id', '!=', $cache['standby'])
 			->as_object()
 			->execute();
 		

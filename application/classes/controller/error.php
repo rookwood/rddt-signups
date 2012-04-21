2aaf8ade35b69b51627f7860b5940e3b8502787c
Matt Hollis <matthollis@gmail.com>
2012-04-21 14:00:02 -0500

    ...

---
+++ application/classes/controller/error.php
@@ -0,0 +1,18 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+class Controller_Error extends Controller {
+
+	public function action_404()
+	{
+		$this->response->body('404\'d');
+	}
+	public function action_503()
+	{
+		$this->response->body('Maintenance Mode');
+	}
+	 
+	public function action_500()
+	{
+		$this->response->body('Internal Server Error');
+	}
+}
